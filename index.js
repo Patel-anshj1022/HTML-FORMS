d4eb363a7a0b9c2912973bae30a2f60a9b86e766
Patel-anshj1022 <patelanshj950@gmail.com>
2025-02-05 17:26:08 +0300

    update index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+console.log("HTML FORMS")
+
+let details = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: ""
+
+}
+
+const firstName = document.getElementById("firstName")
+const submitButton = document.getElementById("submitBtn")
+const lastName = document.getElementById("lastName")
+const email = document.getElementById("email")
+const phoneNumber = document.getElementById("phoneNumber")
+const password = document.getElementById("password")
+const confirmPassword = document.getElementById("confirmPassword")
+firstName.addEventListener("change", (e) => {
+const name = e.target.value
+details.firstName = name
+})
+
+lastName.addEventListener("change", (e) => {
+    const name = e.target.value
+    details.lastNameName = name
+    })
+
+email.addEventListener("change", (e) => {
+    const email = e.target.value
+    details.email = email
+})
+
+phoneNumber.addEventListener("change", (e) => {
+    const phone = e.target.value
+    details.phoneNumber = phone
+})
+
+password.addEventListener("change", (e) => {
+    const password = e.target.value
+    details.password = password
+})
+
+confirmPassword.addEventListener("change", (e) => {
+    const confirmPassword = e.target.value
+    details.confirmPassword = confirmPassword
+})
+
+submitButton.addEventListener("click", () => {
+    console.log(details)
+})
+
+
+
